5ed2d64654799981b0af8b7caf7b0469eb390b82
OctavioMangabeira <octavio.mangabeira@gmail.com>
2020-02-11 13:24:53 -0300

    changes to deploy in heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+require("dotenv").config();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack8-vl593.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -29,6 +30,10 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
+app.use("/", express.static(__dirname + "/../../frontend/dist"));
 app.use(routes);
+const port = process.env.PORT || 9999;
 
-server.listen(3333);
+server.listen(port, function () {
+    console.log(`Servidor executando em ${port}`)
+});
